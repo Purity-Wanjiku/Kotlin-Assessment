c92b5065fbd93c6cd04f9cca91d9674ab5ac7ee9
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-03-22 17:05:14 +0300

    made corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main() {
-text("Suee","Bee")
-    numbers(3,2,1)
+println(text(" "," "))
+    println( numbers(arrayOf(2,4,5,78,90)))
     words("Banana")
+    println(all(arrayOf("rye", "boy","teach")))
 
     var calculate = Calculator(20,10,2)
     calculate.addition()
@@ -10,20 +11,30 @@ text("Suee","Bee")
     calculate.multiplication()
 }
 //NO. 1
-fun text (text1:String,text2:String){
-    if (text1>text2){
-        println(text1[0])
-    }else{
-        (text1==text2)
-        println(text1[0])
-}}
-//N0. 2
-fun numbers (num1:Int,num2:Int,num3:Int){
-    var nums = arrayOf(num1,num2,num3)
-    println(nums.average())
-//    println(arrayOf(nums.maxOf()))
-//    { x -> (x>x) })
+fun text (text1:String,text2:String): Any {
+//   return when {
+//       text1.length > text2.length -> text1[0]
+//       text2.length > text1.length -> text1[0]
+//       text2.length == text1.length -> text1[0]
+//       else -> 'n'
 
+//   }}
+    if (text1.length > text2.length){
+        return (text1[0])
+    }else if (text1.length > text2.length) {
+        return text2[0]
+    }else if (text1.length == text2.length) {
+        return text2[0]
+    }else {
+        return ("no")
+}
+}
+//N0. 2
+fun numbers (num:Array<Int>): Triple<Int, Int, Double>{
+    var num1 = num.max()
+    var num2 = num.min()
+    var num3 = num.average()
+    return Triple(num1,num2, num3)
 }
 //NO. 3
 fun words (word:String){
@@ -31,12 +42,9 @@ fun words (word:String){
 
 }
 //NO. 4
-//fun all (){
-//    var words = arrayOf("word1","word2","word3")
-//    println(words.toString())
-//
-//}
-
+fun all (word:Array<String>):String{
+    return word.joinToString(" ")
+}
 //NO. 5
 class Calculator(var nums1: Int,var nums2: Int,var nums3: Int){
 
